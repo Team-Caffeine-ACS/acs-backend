3be11032155d28b6b5c9a31eed6442e7eee25319
ilja_sokolov <iljasokolov523@gmail.com>
2026-03-13 15:04:16 +0200

    Initial creation of image for backed, with spring boot and java sstructure included

---
+++ src/main/java/com/example/acs_backend/AcsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.acs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsBackendApplication.class, args);
+	}
+
+}
